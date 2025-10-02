368597b1236a2e6144250f9364df965eaeed0bc1
Vikram Rawat <vickey.coool@gmail.com>
2025-10-02 17:58:25 +0530

    add deamons to the plumber

--- global.R
+++ global.R
@@ -1,2 +1,4 @@
 source("dependencies.R")
 source("funcs/dm_funcs.R")
+# section: ----------------------------------
+daemons(8L, dispatcher = FALSE)
--- plumber.R
+++ plumber.R
@@ -5,4 +5,13 @@ main_file <- "routes/main_routes.R"
 
 plumb(main_file) |>
   pr_set_debug(TRUE) |>
+  pr_hook(
+    "exit",
+    function() {
+      message("Plumber app shutting down. Stopping mirai daemons...")
+      # This function terminates all currently running daemons
+      daemons(0)
+      message("mirai daemons stopped.")
+    }
+  ) |>
   pr_run(port = 8000)
