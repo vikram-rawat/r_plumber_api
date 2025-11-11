6601b4a22af90c916b4c562113c8486730ba164c
Vikram Rawat <vickey.coool@gmail.com>
2025-11-11 13:25:31 +0530

    refactor create_dummy_data.R to establish database connection and write iris data

--- api/scripts/create_dummy_data.R
+++ api/scripts/create_dummy_data.R
@@ -1,9 +1,44 @@
-renv::install("adbcsqlite")
+# load libraries: ----------------------------------
 
-renv::update()
-Y
-renv::snapshot()
-Y
+source("api/dependencies.R")
 
-renv::upgrade()
-Y
+# create a db: ----------------------------------
+# Open a new connection to a database
+db <- adbc_database_init(
+  adbcsqlite::adbcsqlite(),
+  uri = "api/data/dummy_data.db"
+)
+
+# establish connection
+conn <- adbc_connection_init(db)
+
+# write iris data to the database
+conn |>
+  write_adbc(
+    tbl = iris,
+    db_or_con = _,
+    target_table = "iris_data"
+  )
+
+# append iris data multiple times to create a large dataset
+for (i in 1:4e3) {
+  if (i %% 100 == 0) {
+    print(i)
+    print(Sys.time())
+  }
+
+  conn |>
+    write_adbc(
+      tbl = iris,
+      db_or_con = _,
+      target_table = "iris_data",
+      mode = "append"
+    )
+}
+
+# convert to file size in MB
+file.info("api/data/dummy_data.db")$size / 2^20 # check file size
+
+# release all connection: ----------------------------------
+adbc_connection_release(conn)
+adbc_database_release(db)
