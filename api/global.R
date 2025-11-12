4a0cce212ae7b629e0c9219704b3ac8d356594be
Vikram Rawat <vickey.coool@gmail.com>
2025-11-12 17:38:44 +0530

    fix: remove unnecessary source call and add setup status print

--- api/global.R
+++ api/global.R
@@ -12,9 +12,11 @@ sqlite_db <<- sqlite_mng("data/dummy_data.db") |>
   # setup the global environment of the daemon
   setup_status <- everywhere({
     source("dependencies.R")
-    source("funcs/data_funcs/dm_funcs.R")
     source("funcs/classes/db_class.R")
     sqlite_db <<- sqlite_mng("data/dummy_data.db") |>
       db_connect()
   })
 }
+
+# check setup status
+print(setup_status[[1]]$data)
